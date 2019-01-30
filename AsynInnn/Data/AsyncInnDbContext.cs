46442dae1472a96a2e6591e412c5ab1c0e5f0c2c
Tulip Pink <pinktulips@hotmail.com>
2019-01-29 19:02:14 -0800

    dude

---
+++ AsynInnn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,32 @@
+﻿using AsynInnn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsynInnn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+            // no contect needed in this constructor, just needs it available 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //composite key associations
+            modelBuilder.Entity<HotelRoom>().HasKey(ce => new { ce.HotelID, ce.RoomNumber });
+            modelBuilder.Entity<RoomAmenities>().HasKey(ce => new { ce.AmenitiesID, ce.RoomID });
+
+            //DATA SEEDING GOES HERE
+        }
+
+        public DbSet<Amenities> Amenities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Room> Room { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
+    }
+}
