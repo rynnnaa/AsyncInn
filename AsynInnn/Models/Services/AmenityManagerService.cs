fb567202d5e1a2042fbf47548f7cff46f6b926f1
Tulip Pink <pinktulips@hotmail.com>
2019-02-10 12:00:02 -0800

    set up amenity

---
+++ AsynInnn/Models/Services/AmenityManagerService.cs
@@ -0,0 +1,20 @@
+﻿using AsynInnn.Models.Interfaces;
+using AsynInnn.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsynInnn.Models.Services
+{
+    public class AmenityManagerService : IAmenityManager
+    {
+        private AsyncInnDbContext _context { get; }
+
+        public AmenityManagerService(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
